c75afeb109fb67ca40c3d42bc1721068bc8190d5
tomwells46 <48603744+tomwells46@users.noreply.github.com>
2019-06-26 22:18:45 +0100

    opacity

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -3,3 +3,10 @@ document.getElementById('sea').addEventListener("click", myFunction);
 function myFunction() {
   alert ("Hello World!");
 }
+
+document.getElementById('wildfire').addEventListener("mouseover", myFunction2);
+
+function myFunction2() {
+  let wildfire = document.getElementById('wildfire');
+  wildfire.style.opacity = '0.1';
+}
